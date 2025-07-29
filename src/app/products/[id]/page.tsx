a8f6fdb0c743fcc37aa0fa15a9ffa51253fd1615
Sandeep M <sandeep723ms@gmail.com>
2025-07-29 10:50:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -176,5 +176,6 @@ function ProductDetailClient({ id }: { id: string }) {
 }
 
 export default function ProductDetailPage({ params }: { params: { id: string } }) {
-  return <ProductDetailClient id={params.id} />;
+  const { id } = params;
+  return <ProductDetailClient id={id} />;
 }

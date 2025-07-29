a94380aa8de5e93778c9907a5ea7d09002a1b6e7
Sandeep M <sandeep723ms@gmail.com>
2025-07-29 11:51:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="absolu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const iconsReinvented = [
   },
   {
     title: "Hoodies",
-    image: "https://placehold.co/600x600",
+    image: "https://images.unsplash.com/photo-1618333262686-d0bdf1e9089f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG9vZGllc3xlbnwwfHwwfHx8MA%3D%3D",
     hint: "cozy hoodie",
   },
   {

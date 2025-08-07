59fe7af3b72a7e5019cff0c39053a46341f118d9
Sandeep M <sandeep723ms@gmail.com>
2025-08-07 16:38:24 +0000

    Check and Update next.config.js:
    Ensure your next.config.js includes st

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+
 import type {NextConfig} from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: isProd ? '/Lazywear' : '',
+  assetPrefix: isProd ? '/Lazywear/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +15,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',

8e7b6774829b46cc2b5bee7b33910b12d38782f8
Sandeep M <sandeep723ms@gmail.com>
2025-10-16 09:03:27 +0000

    fix the following:
    npx tsc --noEmit
    next.config.ts:28:5 - error TS2353:

---
+++ src/next.config.ts
@@ -0,0 +1,33 @@
+
+import type {NextConfig} from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  basePath: isProd ? '/Lazywear' : '',
+  assetPrefix: isProd ? '/Lazywear/' : '',
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ],
+  },
+  trailingSlash: true,
+  allowedDevOrigins: [
+      "https://*.cloudworkstations.dev",
+      "https://*.firebase.studio"
+  ]
+};
+
+export default nextConfig;

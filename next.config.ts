72333db136c63572796ca08df1004d37379eefd4
Sandeep M <sandeep723ms@gmail.com>
2025-11-05 09:35:00 +0000

    Add these keys to your .env or create the file if it doesn't exist. Retr

--- next.config.ts
+++ next.config.ts
@@ -21,16 +21,6 @@ const nextConfig: NextConfig = {
       "https://*.cloudworkstations.dev",
       "https://*.firebase.studio"
   ],
-  webpack: (config, { isServer, webpack }) => {
-    if (!isServer) {
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY': JSON.stringify(process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY),
-        })
-      );
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
